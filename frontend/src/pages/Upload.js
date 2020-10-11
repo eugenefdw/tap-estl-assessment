938ff47d025c1ca2acda6ab625d4820e58dc961a
eugenefdw <eugenefoodw@gmail.com>
2020-10-11 23:47:30 +0800

    Add csv file upload for frontend

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -1,10 +1,49 @@
 import React from 'react';
-import { CSVReader } from 'react-papaparse';
+import axios from 'axios';
+import { DropzoneArea } from 'material-ui-dropzone';
+import { Button } from '@material-ui/core';
 
 export default function Upload() {
+  const [state, setState] = React.useState({
+    files: []
+  });
+
+  const handleDropzoneChange = (inFiles) => {
+    console.log(inFiles);
+    setState((prevState) => ({
+      ...prevState,
+      files: inFiles,
+    }));
+  };
+
+  const submitFiles = () => {
+    var formData = new FormData();
+    state.files.forEach(file => {
+      formData.append('file', file);
+    });
+    console.log('posted');
+    axios.post('http://localhost:3000/users/upload', formData, {headers: {'Content-Type': 'multipart/form-data'}})
+    .then((response) => {
+      console.log(response)
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
+
   return (
     <div>
-      Upload
+      <DropzoneArea 
+      showFileNamesInPreview={true}
+      showPreviews={true}
+      previewText={"Files added:"}
+      showPreviewsInDropzone={false}
+      onChange={handleDropzoneChange}
+      acceptedFiles={[".csv, text/csv, application/vnd.ms-excel, application/csv, text/x-csv, application/x-csv, text/comma-separated-values, text/x-comma-separated-values"]}
+      filesLimit={1}
+      dropzoneText={"Drag and drop or click to upload a csv file"}
+      />
+      <Button variant="contained" color="primary" onClick={submitFiles}>Submit</Button>
     </div>
   )
 }
