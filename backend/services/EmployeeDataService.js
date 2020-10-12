79ea2a90b32dfcc5285002275104dca7e4b7a57f
eugenefdw <eugenefoodw@gmail.com>
2020-10-12 19:13:58 +0800

    Fix minimum salary check

--- backend/services/EmployeeDataService.js
+++ backend/services/EmployeeDataService.js
@@ -1,8 +1,9 @@
 const EmployeesModel = require('../models/EmployeesModel');
 
 exports.getDataAt = function (params, callback) {
-  if(params.minSalary < params.maxSalary) {
-    callback("Minimum salary less than maximum salary", null);
+  if(params.minSalary > params.maxSalary) {
+    callback("Minimum salary more than maximum salary", null);
+    return;
   }
 
   
