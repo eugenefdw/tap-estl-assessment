aa0634834d88e18fe99be1399e2b61d05b9493d0
eugenefdw <eugenefoodw@gmail.com>
2020-10-12 23:20:20 +0800

    Add separate file viewer and upload confirmation

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -1,36 +1,38 @@
 import React from 'react';
 import axios from 'axios';
 import { DropzoneArea } from 'material-ui-dropzone';
-import { Button, Container, makeStyles } from '@material-ui/core';
+import { Button, Container, makeStyles, Paper } from '@material-ui/core';
 
 
 const useStyles = makeStyles((theme) => ({
   dropzone: {
     margin: '25px 0px'
-  }
+  },
+  fileInfo: {
+    marginBottom: '25px',
+  },
 }));
 
 export default function Upload() {
   const classes = useStyles();
-  const [state, setState] = React.useState({
-    files: []
-  });
+  const [files, setFiles] = React.useState([]);
 
   const handleDropzoneChange = (inFiles) => {
-    setState((prevState) => ({
-      ...prevState,
-      files: inFiles,
-    }));
+    setFiles(inFiles);
   };
 
   const submitFiles = () => {
+    if(files.length === 0) {
+      return;
+    }
     var formData = new FormData();
-    state.files.forEach(file => {
+    files.forEach(file => {
       formData.append('file', new File([file], file.name, { type: 'text/csv' }));
     });
     axios.post('http://localhost:2021/users/upload', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
       .then((res) => {
         console.log(res);
+        setFiles([]);
       }).catch((err) => {
         console.error(err);
       });
@@ -40,9 +42,7 @@ export default function Upload() {
   return (
     <Container maxWidth="md" >
       <DropzoneArea
-        showFileNamesInPreview={true}
-        showPreviews={true}
-        previewText={"Files added:"}
+        showPreviews={false}
         showPreviewsInDropzone={false}
         onChange={handleDropzoneChange}
         acceptedFiles={[".csv, text/csv, application/vnd.ms-excel, application/csv, text/x-csv, application/x-csv, text/comma-separated-values, text/x-comma-separated-values"]}
@@ -50,6 +50,13 @@ export default function Upload() {
         dropzoneText={"Drag and drop or click to upload a csv file"}
         dropzoneClass={classes.dropzone}
       />
+      {files.map((file, index) => {
+        return (
+          <Paper key={index} className={classes.fileInfo}>
+            {file.name}
+          </Paper>
+        )
+      })}
       <Button variant="contained" color="primary" onClick={submitFiles}>Submit</Button>
     </Container>
   )
