a2e2c77f6381f03eed7006be30bcff6b7828074c
eugenefdw <eugenefoodw@gmail.com>
2020-10-12 18:20:38 +0800

    Refactor name of parser service

--- backend/routes/UploadRouter.js
+++ backend/routes/UploadRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const parser = require('../services/CsvParser');
+const parser = require('../services/CsvParserService');
 
 var isUploading = false;
 
