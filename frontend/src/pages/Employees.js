7b0ec497d623a814ac52e73b410a1689f3bb3b8c
eugenefdw <eugenefoodw@gmail.com>
2020-10-12 17:21:13 +0800

    Add skeleton for employee dashboard

--- frontend/src/pages/Employees.js
+++ frontend/src/pages/Employees.js
@@ -1,9 +1,39 @@
+import { Grid, makeStyles, Paper } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  container: {
+    margin: '25px 0px 0px 0px'
+  },
+  salaryFilterRow: {
+
+  },
+}));
+
 export default function Employees() {
+  const classes = useStyles();
   return (
-    <div>
-      Employees
+    <div className={classes.root}>
+      <Grid container xs={12} className={classes.container} alignItems="center" justify="center">
+        <Grid container item xs={12} spacing={4} alignItems="center" justify="center">
+          <Grid item md={6} sm={12}>
+            <Paper>
+              FILTER
+            </Paper>
+          </Grid>
+          <Grid item md={6} sm={12} >
+            <Paper>
+              FILTER
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+        </Grid>
+      </Grid>
     </div>
+
   )
 }
