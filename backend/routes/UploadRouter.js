cbe5a070a89ec6339cc0d10d854bc01f3cc81949
eugenefdw <eugenefoodw@gmail.com>
2020-10-12 14:45:47 +0800

    Add file upload blocking

--- backend/routes/UploadRouter.js
+++ backend/routes/UploadRouter.js
@@ -2,16 +2,22 @@ const express = require('express');
 const router = express.Router();
 const parser = require('../services/CsvParser');
 
+var isUploading = false;
+
 router.post('/users/upload', (req, response) => {
   if (!req.files) {
-    response.send({
-      status: false,
-      message: 'No file uploaded'
-    });
+    response.sendStatus(200);
     return;
   }
 
+  if(isUploading) {
+    response.sendStatus(500);
+    return;
+  }
+
+  isUploading = true;
   parser.parseCsvFile(req.files.file, (error, res) => {
+    isUploading = false;
     if (error) {
       response.sendStatus(500);
     } else {
