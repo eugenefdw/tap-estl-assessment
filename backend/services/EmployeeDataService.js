a13ee598f8c52be2402018dd224d9da162f15f0a
eugenefdw <eugenefoodw@gmail.com>
2020-10-12 19:14:18 +0800

    Add request validation

--- backend/services/EmployeeDataService.js
+++ backend/services/EmployeeDataService.js
@@ -6,5 +6,23 @@ exports.getDataAt = function (params, callback) {
     return;
   }
 
-  
+  if(params.limit > 30) {
+    callback("Maximum query size is 30", null);
+    return;
+  }
+
+  //get asc/desc from first char of sort string
+  if(params.sort.charAt(0) === '+') {
+    params.ascending = true;
+  } else if(params.sort.charAt(0) === '-') {
+    params.ascending = false;
+  } else {
+    callback("Invalid sort type", null);
+    return;
+  }
+
+  //get remaining column type from sort string
+  params.column = params.sort.substring(1);
+
+
 }
