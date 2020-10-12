df016d9b5f0f85c9023fc2550914b45c19c878a7
eugenefdw <eugenefoodw@gmail.com>
2020-10-12 16:22:17 +0800

    Update upload page styling

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import axios from 'axios';
 import { DropzoneArea } from 'material-ui-dropzone';
-import { Button } from '@material-ui/core';
+import { Box, Button, Container, makeStyles } from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+  dropzone: {
+    margin: '25px 0px'
+  }
+}));
 
 export default function Upload() {
+  const classes = useStyles();
   const [state, setState] = React.useState({
     files: []
   });
@@ -19,30 +27,31 @@ export default function Upload() {
   const submitFiles = () => {
     var formData = new FormData();
     state.files.forEach(file => {
-      formData.append('file', new File([file], file.name, {type: 'text/csv'}));
+      formData.append('file', new File([file], file.name, { type: 'text/csv' }));
     });
-    axios.post('http://localhost:2021/users/upload', formData, {headers: {'Content-Type': 'multipart/form-data'}})
-    .then((response) => {
-      console.log(response);
-    }).catch((error) => {
-      console.error(error);
-    });
+    axios.post('http://localhost:2021/users/upload', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+      .then((response) => {
+        console.log(response);
+      }).catch((error) => {
+        console.error(error);
+      });
   }
 
 
   return (
-    <div>
-      <DropzoneArea 
-      showFileNamesInPreview={true}
-      showPreviews={true}
-      previewText={"Files added:"}
-      showPreviewsInDropzone={false}
-      onChange={handleDropzoneChange}
-      acceptedFiles={[".csv, text/csv, application/vnd.ms-excel, application/csv, text/x-csv, application/x-csv, text/comma-separated-values, text/x-comma-separated-values"]}
-      filesLimit={1}
-      dropzoneText={"Drag and drop or click to upload a csv file"}
+    <Container maxWidth="md" >
+      <DropzoneArea
+        showFileNamesInPreview={true}
+        showPreviews={true}
+        previewText={"Files added:"}
+        showPreviewsInDropzone={false}
+        onChange={handleDropzoneChange}
+        acceptedFiles={[".csv, text/csv, application/vnd.ms-excel, application/csv, text/x-csv, application/x-csv, text/comma-separated-values, text/x-comma-separated-values"]}
+        filesLimit={1}
+        dropzoneText={"Drag and drop or click to upload a csv file"}
+        dropzoneClass={classes.dropzone}
       />
       <Button variant="contained" color="primary" onClick={submitFiles}>Submit</Button>
-    </div>
+    </Container>
   )
 }
