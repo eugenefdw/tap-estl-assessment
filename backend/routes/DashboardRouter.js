072d13b67a1349417627e8a1837d97f68ce6eada
eugenefdw <eugenefoodw@gmail.com>
2020-10-12 18:25:51 +0800

    Add employee dashboard API routing

--- backend/routes/DashboardRouter.js
+++ backend/routes/DashboardRouter.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const dataService = require('../services/EmployeeDataService');
 
 router.get('/users', function (request, response) {
-  const params = request.params;
-  retrieveEmployeeData(function (error, users) {
-    if (error) {
-      response.sendStatus(500);
-    } else {
-      response.send(JSON.stringify(users));
-    }
-  });
+  const params = request.query;
+  dataService.getDataAt(
+    params.minSalary,
+    params.maxSalary,
+    params.offset,
+    params.limit,
+    params.sort,
+    (err, res) => {
+      if (err) {
+        response.sendStatus(500);
+      } else {
+        response.sendStatus(200);
+      }
+    });
 });
 
 module.exports = router;
---
+++ backend/services/EmployeeDataService.js
@@ -0,0 +1,5 @@
+const EmployeesModel = require('../models/EmployeesModel');
+
+exports.getDataAt = function (minSalary, maxSalary, offset, limit, sort, callback) {
+
+}
